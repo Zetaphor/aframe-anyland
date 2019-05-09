b77911384bb655a1607d3fb512fde48b21149e89
Gary Daigle <zetaphor@gmail.com>
2019-05-09 02:42:09 -0400

    Add generateUID to store

--- src/store/index.js
+++ src/store/index.js
@@ -30,6 +30,12 @@ export default new Vuex.Store({
   },
 
   getters: {
+    generateUid: function () {
+      return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+      )
+    },
+
     raycasterLine: state => {
       return `color: ${state.laserColor};`
     }
