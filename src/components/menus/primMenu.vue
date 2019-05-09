51fe9eb23910b0db741e1e5e857458ffa7975148
Gary Daigle <zetaphor@gmail.com>
2019-05-09 02:42:56 -0400

    Move generate UID to store

--- src/components/menus/primMenu.vue
+++ src/components/menus/primMenu.vue
@@ -79,15 +79,9 @@ export default {
       this.$set(this.prims[index], 'color', '#FFF')
     },
 
-    generatePrimId: function () {
-      return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-      )
-    },
-
     create: function (type) {
-      const newPrim = document.createElement('a-entity')
-      const newId = 'prim-'+ this.generatePrimId()
+      let newPrim = document.createElement('a-entity')
+      let newId = 'prim-'+ this.$store.getters.generateUid
 
       newPrim.setAttribute('mixin', 'prim-' + type)
       newPrim.setAttribute('id', newId)
