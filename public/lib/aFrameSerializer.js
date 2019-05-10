424a5139446d9df7d8dfdc6dc435157271b5a874
Gary Daigle <zetaphor@gmail.com>
2019-05-10 07:16:19 -0400

    Added deserializeJson

--- public/lib/aFrameSerializer.js
+++ public/lib/aFrameSerializer.js
@@ -1,15 +1,20 @@
 const componentMaps = {
   position: ['x', 'y', 'z'],
+  geometry: ['width', 'height', 'depth', 'primitive', 'radius', 'radius-bottom', 'radius-top'],
+  scale: true,
+  'body-merger': true,
   class: true,
   'collision-filter': true,
   'dynamic-body': true,
-  geometry: ['width', 'height', 'depth', 'primitive'],
+  'static-body': true,
+  'static-grabbable': true,
   grabbable: false,
   material: true,
   scale: true,
   sleepy: false,
   stretchable: false,
-  hoverable: false
+  hoverable: false,
+  draggable: false
 }
 
 window.aFrameSerialize = function (entityId) {
@@ -18,14 +23,14 @@ window.aFrameSerialize = function (entityId) {
   }
 
   let entity = document.querySelector('#' + entityId)
-  // entityJson.root = serializePrim(entity)
+  entityJson.root = serializePrim(entity)
   if (entity.childNodes.length) {
     entityJson['children'] = []
     for (let index = 0; index < entity.childNodes.length; index++) {
       entityJson.children.push(serializePrim(entity.childNodes[index]))
     }
   }
-  console.log(entityJson)
+  return entityJson
 }
 
 function serializePrim(entity) {
@@ -35,10 +40,13 @@ function serializePrim(entity) {
       if (componentMaps[entity.attributes[index].name] === false) {
         primJson[entity.attributes[index].name] = ''
       } else if (componentMaps[entity.attributes[index].name] === true) {
-        primJson[entity.attributes[index].name] = entity.getAttribute(entity.attributes[index].name)
+        primJson[entity.attributes[index].name] = entity.getAttribute(entity.attributes[index].name) || ''
       } else {
         for (let keyIndex = 0; keyIndex < componentMaps[entity.attributes[index].name].length; keyIndex++) {
-          primJson[componentMaps[entity.attributes[index].name][keyIndex]] = entity.getAttribute(entity.attributes[index].name)[componentMaps[entity.attributes[index].name][keyIndex]]
+          if (typeof entity.getAttribute(entity.attributes[index].name) === 'undefined') continue
+          if (typeof entity.getAttribute(entity.attributes[index].name)[componentMaps[entity.attributes[index].name][keyIndex]] === 'undefined') continue
+          if (typeof primJson[entity.attributes[index].name] === 'undefined') primJson[entity.attributes[index].name] = {}
+          primJson[entity.attributes[index].name][componentMaps[entity.attributes[index].name][keyIndex]] = entity.getAttribute(entity.attributes[index].name)[componentMaps[entity.attributes[index].name][keyIndex]]
         }
       }
     }
@@ -47,5 +55,27 @@ function serializePrim(entity) {
 }
 
 window.aFrameDeserialize = function (objectJson) {
+  let newObject = document.createElement('a-entity')
+  newObject.setAttribute('id', 'obj-' + window.generateUid())
+  newObject = deserializePrim(objectJson.root)
+  for (let index = 0; index < objectJson.children.length; index++) {
+    newObject.appendChild(deserializePrim(objectJson.children[index]))
+  }
+  document.querySelector('#scene').appendChild(newObject)
+  newObject.addEventListener('body-loaded', function () {
+    newObject.body.updateBoundingRadius()
+    newObject.body.updateMassProperties()
+    newObject.body.wakeUp()
+  })
+  return newObject
+}
 
+function deserializePrim(primJson) {
+  let newPrimId = 'prim-' + window.generateUid()
+  let newPrim = document.createElement('a-entity')
+  newPrim.setAttribute('id', newPrimId)
+  for (const attribute in primJson) {
+    newPrim.setAttribute(attribute, primJson[attribute])
+  }
+  return newPrim
 }
