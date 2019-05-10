2bcb40b4b268d229323b2481b79950a82d1ce5ed
Gary Daigle <zetaphor@gmail.com>
2019-05-10 03:30:24 -0400

    Set new prim collision filters

--- src/components/menus/assetMixinMenuPrims.vue
+++ src/components/menus/assetMixinMenuPrims.vue
@@ -3,21 +3,21 @@
     <a-mixin id="prim-box"
       geometry="primitive: box; width: 0.2; height: 0.2; depth: 0.2"
       material="color:#FFF" class="collidable"
-      :collision-filter="`group: touchable; collidesWith:${$store.state.objectCollisionFilter}`"
+      :collision-filter="`group: touchable; collidesWith:${$store.state.newPrimCollisionFilter}`"
       static-body hoverable static-grabbable
       grabbable stretchable></a-mixin>
 
     <a-mixin id="prim-sphere"
       geometry="primitive: sphere; radius:0.1"
       material="color:#FFF" class="collidable"
-      :collision-filter="`group: touchable; collidesWith:${$store.state.objectCollisionFilter}`"
+      :collision-filter="`group: touchable; collidesWith:${$store.state.newPrimCollisionFilter}`"
       static-body hoverable static-grabbable
       grabbable stretchable></a-mixin>
 
     <a-mixin id="prim-cone"
       geometry="primitive: cone; radius-bottom:0.0; radius-top:0.15; height:0.2"
       material="color:#FFF" class="collidable"
-      :collision-filter="`group: touchable; collidesWith:${$store.state.objectCollisionFilter}`"
+      :collision-filter="`group: touchable; collidesWith:${$store.state.newPrimCollisionFilter}`"
       static-body hoverable static-grabbable
       grabbable stretchable></a-mixin>
   </div>
